188c3c51ba930e285bc461861667a2bfbe0ddfe0
Gustavo Oliveira <gustavo.silva@teknisa.com>
2022-03-16 20:59:58 -0300

    Mod - Adicionado alterações do escopo.

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,47 @@
                 </div>
             </div>
         </div>
+        <div>
+            <h4 v-text="devs.length + ' resultados'"/>
+        </div>
+        <div class="listagem">
+            <div class="card" v-for="dev in devs" :key="dev.key">
+                <div>
+                    <img class="user-image" :src="dev.picture">
+                </div>
+                <div class="info">
+                    <h4 class="name" v-text="dev.name"/>
+                    <span class="age" v-text="dev.age + ' anos'"/>
+                    <span class="email" v-text="dev.email"/>
+                    <div>
+                        <img class="languages" v-for="lang in dev.programmingLanguages" :key="lang.key" :src="imgLang[lang.id]">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+      return {
+            devs: [],
+            imgLang: {
+                'JavaScript': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/640px-Unofficial_JavaScript_logo_2.svg.png',
+                'Python': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/800px-Python-logo-notext.svg.png',
+                'Java': 'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg',
+                'Php': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/800px-PHP-logo.svg.png'
+            }
+      }
+  },
+  created () {
+    axios.get('https://bernardosantos.zeedhi.com/workfolder/dev.php').then((response) => {
+        this.devs = response.data.devs
+    })
+  }
 }
 </script>
 
@@ -58,5 +93,14 @@ label {
 }
 .linguagens {
     display: inline-block;
+}
+.card {
+    width:500px;
+    display: grid;
+    grid-template-columns: 28% 72%;
+}
+.languages {
+    width:30px;
+    height: 30px;;
 }
 </style>
